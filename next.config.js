fa304c7a7083e3fc8e19456e842306315fde125c
Shujaat Khan <se.shujaatkhan@gmail.com>
2023-12-03 02:56:31 +0500

    done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
+    images: {
+      domains: [
+        "uploadthing.com",
+        "utfs.io"
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
